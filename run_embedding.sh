f49eff8894239de36446dbd63792d7c49bd42200
francosimonetti <francolsimonetti@gmail.com>
2022-09-13 08:53:06 -0300

    add notebook for plotting umaps

---
+++ run_embedding.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash -e
+
+#### 1. Calculate embeddings
+## obtain embeddings for a fasta dir or multi fasta file
+
+#python calc_embeddings.py --model halft5 --fastadir /data/franco/datasets/disprot/fasta --outdir disprot
+#python calc_embeddings.py --model halft5 --fasta /data/franco/datasets/disprot/monomers.fasta --outdir monomers
+#python calc_embeddings.py --model esmb1 --fasta /data/franco/datasets/disprot/monomers.fasta --outdir monomers
+
+
+
+#### 2. Prepare data
+## Prepares seq_data, annot_data and embedding data by concatenating sequences, annotations and embedding matrices
+## Makes sure that the fasta matches dimensions of the annotations and embeddings
+
+#python prepare_plotdata.py --model halft5 --fasta /data/franco/datasets/disprot/monomers.fasta --annot /data/franco/datasets/disprot/monomers_annot.fasta --embeddir monomers/halft5/ --outdir ./monomers_plotdata
+#python prepare_plotdata.py --model halft5 --fastadir /data/franco/datasets/disprot/fasta/ --annot /data/franco/datasets/disprot/DisProt_release_2022_06_reformat_annot.fasta --embeddir disprot/halft5/ --outdir ./disprot_plotdata
+
+
+
+#### 3. Calculate UMAP
+### Don't run just now
+## This was run on the cluster, takes too long
+
+# NEIGHBOURS="5 10 30 60 100 200"
+# DISTS="0.001 0.01 0.1 0.25 0.5 0.9"
+
+# for N in $NEIGHBOURS; do
+#     for D in $DISTS; do
+#         echo $N $D;
+#         python calc_umap.py --embedmat ./disprot_plotdata/halft5/embedding_data.txt.npz --neighbours $N --mindist $D --outfile ./disprot_plotdata/halft5/umap_${N}_${D}.txt
+#     done;
+# done;
+
+########################
+
+#### 4. Make UMAP plots
+## load UMAP coordinates from previous step and plot scatter plots with seq and annot data
+
